be1577ef02b9e13a96f3d1b4b5db59523d2a0951
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-01 18:56:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,7 +1,7 @@
 WIN_COMBINATIONS.each do |win_combo|
-+      if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X") || (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
-+        return win_combo
-+      end
-+    end
-+    return false
-+  end
+     if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X") || (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
+        return win_combo
+      end
+   end
+ return false
+  end
