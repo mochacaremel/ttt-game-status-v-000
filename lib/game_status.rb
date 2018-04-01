5a9913c40199c2f6a20f77ea8cf2f5b356309fe5
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-01 18:51:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,252 +1,227 @@
-class Tic
-
-  def initialize
-    #map of all places that are possible wins
-    @columns = [
-      [:a1,:a2,:a3],
-      [:b1,:b2,:b3],
-      [:c1,:c2,:c3],
-
-      [:a1,:b1,:c1],
-      [:a2,:b2,:c2],
-      [:a3,:b3,:c3],
-
-      [:a1,:b2,:c3],
-      [:c1,:b2,:a3]
-    ]
-
-    @cpu = rand() > 0.5 ? 'X' : 'O'
-    @user = @cpu == 'X' ? 'O' : 'X'
-
-    @cpu_name = "Ruby"
-    put_line
-    puts "\n  RUBY TIC TAC TOE".purple
-    print "\n What is your name? ".neon
-    STDOUT.flush
-    @user_name = gets.chomp
-    put_bar
-
-    @user_score = 0
-    @cpu_score = 0
-
-    start_game(@user == 'X')
-  end
-
-  def start_game(user_goes_first)
-    #the tic tac toe slots
-    @places = {
-      a1:" ",a2:" ",a3:" ",
-      b1:" ",b2:" ",b3:" ",
-      c1:" ",c2:" ",c3:" "
-    }
-
-    if user_goes_first
-      user_turn
-    else
-      cpu_turn
+class TicTacToe
+    # initialize
+    def initialize
+        # set up the board
+        <@UNVERIFIED|@board> = Board.new
+        # set up the players
+        @player_x = Player.new("Madame X", :x, <@UNVERIFIED|@board>)
+        @player_y = Player.new("Mister Y", :y, <@UNVERIFIED|@board>)
+        # assign the starting player
+        @current_player = @player_x
     end
-  end
-
-  def restart_game(user_goes_first)
-    (1...20).each { |i| put_line }
-    start_game(user_goes_first)
-  end
-
-  def put_line
-    puts ("-" * 80).gray
-  end
-
-  def put_bar
-    puts ("#" * 80).gray
-    puts ("#" * 80).gray
-  end
-
-  def draw_game
-    puts ""
-    puts " Wins: #{@cpu_name}:#{@cpu_score} #{@user_name}:#{@user_score}".gray
-    puts ""
-    puts " #{@cpu_name}: #{@cpu.green}"
-    puts " #{@user_name}: #{@user.green}"
-    puts ""
-    puts "     a   b   c".gray
-    puts ""
-    puts " 1   #{@places[:a1].green} | #{@places[:b1].green} | #{@places[:c1].green} ".gray
-    puts "    --- --- ---"
-    puts " 2   #{@places[:a2].green} | #{@places[:b2].green} | #{@places[:c2].green} ".gray
-    puts "    --- --- ---"
-    puts " 3   #{@places[:a3].green} | #{@places[:b3].green} | #{@places[:c3].green} ".gray
-  end
-
-  def cpu_turn
-    move = cpu_find_move
-    @places[move] = @cpu
-    put_line
-    puts " #{@cpu_name} marks #{move.to_s.upcase.green}".neon
-    check_game(@user)
-  end
-
-  def cpu_find_move
-
-    # see if cpu can win
-    #see if any columns already have 2 (cpu)
-    @columns.each do |column|
-      if times_in_column(column, @cpu) == 2
-        return empty_in_column column
-      end
-    end
-
-    # see if user can win
-    #see if any columns already have 2 (user)
-    @columns.each do |column|
-      if times_in_column(column, @user) == 2
-        return empty_in_column column
-      end
-    end
-
-    #see if any columns aready have 1 (cpu)
-    @columns.each do |column|
-      if times_in_column(column, @cpu) == 1
-        return empty_in_column column
-      end
-    end
-
-    #no strategic spot found so just find a random empty
-    k = @places.keys;
-    i = rand(k.length)
-    if @places[k[i]] == " "
-      return k[i]
-    else
-      #random selection is taken so just find the first empty slot
-      @places.each { |k,v| return k if v == " " }
-    end
-  end
-
-  def times_in_column arr, item
-    times = 0
-    arr.each do |i|
-      times += 1 if @places[i] == item
-      unless @places[i] == item || @places[i] == " "
-        #oppisite piece is in column so column cannot be used for win.
-        #therefore, the strategic thing to do is choose a dif column so return 0.
-        return 0
-      end
-    end
-    times
-  end
-
-  def empty_in_column arr
-    arr.each do |i|
-      if @places[i] == " "
-        return i
-      end
-    end
-  end
-
-  def user_turn
-    put_line
-    puts "\n  RUBY TIC TAC TOE".purple
-    draw_game
-    print "\n #{@user_name}, please make a move or type 'exit' to quit: ".neon
-    STDOUT.flush
-    input = gets.chomp.downcase.to_sym
-    put_bar
-    if input.length == 2
-      a = input.to_s.split("")
-      if(['a','b','c'].include? a[0])
-        if(['1','2','3'].include? a[1])
-          if @places[input] == " "
-            @places[input] = @user
-            put_line
-            puts " #{@user_name} marks #{input.to_s.upcase.green}".neon
-            check_game(@cpu)
-          else
-            wrong_move
-          end
-        else
-          wrong_input
+    # play
+    def play
+        # loop infinitely
+        loop do
+            # call the board rendering method
+            @board.render
+            # ask for coordinates from the current player
+            @current_player.get_coordinates
+            # check if game is over
+            break if check_game_over
+            # switch players
+            switch_players
         end
-      else
-        wrong_input
-      end
-    else
-      wrong_input unless input == :exit
     end
-  end
-
-  def wrong_input
-    put_line
-    puts " Please specify a move with the format 'A1' , 'B3' , 'C2' etc.".red
-    user_turn
-  end
-
-  def wrong_move
-    put_line
-    puts " You must choose an empty slot".red
-    user_turn
-  end
-
-  def moves_left
-    @places.values.select{ |v| v == " " }.length
-  end
-
-  def check_game(next_turn)
-
-    game_over = nil
-
-    @columns.each do |column|
-      # see if cpu has won
-      if times_in_column(column, @cpu) == 3
-        put_line
-        draw_game
-        put_line
-        puts ""
-        puts " Game Over -- #{@cpu_name} WINS!!!\n".blue
-        game_over = true
-        @cpu_score += 1
-        ask_to_play_again(false)
-      end
-      # see if user has won
-      if times_in_column(column, @user) == 3
-        put_line
-        draw_game
-        put_line
-        puts ""
-        puts " Game Over -- #{@user_name} WINS!!!\n".blue
-        game_over = true
-        @user_score += 1
-        ask_to_play_again(true)
-      end
+    # check_game_over?
+    def check_game_over
+        # check for victory
+        # check for draw
+        check_victory || check_draw
     end
-
-    unless game_over
-      if(moves_left > 0)
-        if(next_turn == @user)
-          user_turn
+    # check_victory?
+    def check_victory
+        # IF Board says current player's piece has
+        # a winning_combination?
+        if @board.winning_combination?(@current_player.piece)
+            # then output a victory message
+            puts "Congratulations #{@current_player.name}, you win!"
+            true
         else
-          cpu_turn
+            false
         end
-      else
-        put_line
-        draw_game
-        put_line
-        puts ""
-        puts " Game Over -- DRAW!\n".blue
-        ask_to_play_again(rand() > 0.5)
-      end
     end
-  end
-
-  def ask_to_play_again(user_goes_first)
-    print " Play again? (Yn): "
-    STDOUT.flush
-    response = gets.chomp.downcase
-    case response
-    when "y"   then restart_game(user_goes_first)
-    when "yes" then restart_game(user_goes_first)
-    when "n"   then #do nothing
-    when "no"  then #do nothing
-    else ask_to_play_again(user_goes_first)
+    # check_draw?
+    def check_draw
+        # If Board says we've filled up 
+        if @board.full?
+            # display draw message
+            puts "Bummer, you've drawn..."
+            true
+        else
+            false
+        end
     end
-  end
-
+    # switch_players
+    def switch_players
+        if @current_player == @player_x
+            @current_player = @player_y
+        else
+            @current_player = @player_x
+        end
+    end
 end
+# Manages all player-related functionality
+class Player
+    attr_accessor :name, :piece
+    # initialize
+    def initialize(name = "Mystery_Player", piece, board)
+        # Set marker type (e.g. X or O)
+        raise "Piece must be a Symbol!" unless piece.is_a?(Symbol)
+        <@UNVERIFIED|@name> = name
+        @piece = piece
+        <@UNVERIFIED|@board> = board
+    end
+    # get_coordinates
+    def get_coordinates
+        # loop infinitely
+        loop do
+            # ask_for_coordinates
+            coords = ask_for_coordinates
+            # IF validate_coordinates_format is true
+            if validate_coordinates_format(coords)
+                # IF piece can be placed on Board
+                if @board.add_piece(coords, @piece)
+                    # break the loop
+                    break
+                end
+            end
+        end
+    end
+    # ask_for_coordinates
+    def ask_for_coordinates
+        # Display message asking for coordinates
+        puts "#{@name}(#{@piece}), enter your coordinates in the form x,y:"
+        # pull coordinates from command line
+        STDIN.gets.strip.split(",").map(&:to_i)
+    end
+    # validate_coordinates_format
+    def validate_coordinates_format(coords)
+        # UNLESS coordinates are in the proper format
+        if coords.is_a?(Array) && coords.size == 2
+            true
+        else
+            # display error message
+            # Note that returning `nil` acts falsy!
+            puts "Your coordinates are in the improper format!"
+        end
+    end
+end
+# Maintains game board state
+class Board
+    # initialize board
+    def initialize
+        # set up blank data structure
+        <@UNVERIFIED|@board> = Array.new(3){Array.new(3)}
+    end
+    # render
+    def render
+        puts
+        # loop through data structure
+        @board.each do |row|
+            row.each do |cell|
+                # display an existing marker if any, else blank
+                cell.nil? ? print("-") : print(cell.to_s)
+            end
+            puts
+        end
+        puts
+    end
+    # add_piece
+    def add_piece(coords, piece)
+        # IF piece_location_valid?
+        if piece_location_valid?(coords)
+            # place piece
+            @board[coords[0]][coords[1]] = piece
+            true
+        else
+            false
+        end
+    end
+    # piece_location_valid?
+    def piece_location_valid?(coords)
+        # Is the placement within_valid_coordinates?
+        if within_valid_coordinates?(coords)
+            # Are the piece coordinates_available?
+            coordinates_available?(coords)
+        end
+    end
+    # within_valid_coordinates?
+    def within_valid_coordinates?(coords)
+        # UNLESS piece coords are in the acceptible range
+        if (0..2).include?(coords[0]) && (0..2).include?(coords[1])
+            true
+        else
+            # display an error message
+            puts "Piece coordinates are out of bounds"
+        end
+    end
+    # coordinates_available?
+    def coordinates_available?(coords)
+        # UNLESS piece coords are not occupied
+        if @board[coords[0]][coords[1]].nil?
+            true
+        else
+            # display error message
+            puts "There is already a piece there!"
+        end
+    end
+    # winning_combination?
+    def winning_combination?(piece)
+        # is there a winning_diagonal?
+        # or winning_vertical? 
+        # or winning_horizontal? for that piece?
+        winning_diagonal?(piece)   || 
+        winning_horizontal?(piece) || 
+        winning_vertical?(piece)
+    end
+    # winning_diagonal?
+    def winning_diagonal?(piece)
+        # check if specified piece has a triplet across diagonals
+        diagonals.any? do |diag|
+            diag.all?{|cell| cell == piece }
+        end
+    end
+    # winning_vertical?
+    def winning_vertical?(piece)
+        # check if specified piece has a triplet across verticals
+        verticals.any? do |vert|
+            vert.all?{|cell| cell == piece }
+        end
+    end
+    # winning_horizontal?
+    def winning_horizontal?(piece)
+        # check if specified piece has a triplet across horizontals
+        horizontals.any? do |horz|
+            horz.all?{|cell| cell == piece }
+        end
+    end
+    # diagonals
+    def diagonals
+        # return the diagonal pieces
+        [[ @board[0][0],@board[1][1],@board[2][2] ],[ @board[2][0],@board[1][1],@board[0][2] ]]
+    end
+    # verticals
+    def verticals
+        # return the vertical pieces
+        <@UNVERIFIED|@board>
+    end
+    # horizontals
+    def horizontals
+        # return the horizontal pieces
+        horizontals = []
+        3.times do |i|
+            horizontals << [@board[0][i],@board[1][i],@board[2][i]]
+        end
+        horizontals
+    end
+    # full?
+    def full?
+        # does every square contain a piece?
+        @board.all? do |row|
+            row.none?(&:nil?)
+        end
+    end
+end
+t = TicTacToe.new
+t.play
